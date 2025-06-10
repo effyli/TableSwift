59d83d369cf2760ab20fd59fe3488d376ebec222
Effy <effyli.x@gmail.com>
2025-06-10 11:58:18 +0200

    adding execution and labeling functions

--- src/tableswift/core.py
+++ src/tableswift/core.py
@@ -1,18 +1,27 @@
 from typing import List
 from .config import with_api_key, resolve_hyperparams
 from .logic.codegen import generate_code_logic
+from .logic.execution import execute_code_logic
+from .logic.labeling import label_data_logic
 
 
 @with_api_key
-def generate_labels(instruction: str, input_samples: List[str],api_key=None, **overrides) -> None:
+def generate_labels(instruction: str, task: str, samples_to_label: List, api_key=None, demonstrations: List[dict]=None, **overrides) -> None:
     """
     Generate labels given instructions and input samples.
     """
-    
-    return None
+    # input should be a list of dictionaries
+    hyperparams = resolve_hyperparams(overrides)
+    return label_data_logic(
+        instruction=instruction,
+        task=task,
+        demonstrations=demonstrations,
+        samples_to_label=samples_to_label,
+        api_key=api_key, 
+        hyperparams=hyperparams)
 
 @with_api_key
-def generate_code(instruction: str, task:str, samples: List[List], lang: str, api_key=None, **overrides) -> str:
+def generate_code(instruction: str, task:str, samples: List, lang: str, api_key=None, **overrides) -> str:
     """
     Generate code given instructions and input samples.
     """
@@ -26,9 +35,21 @@ def generate_code(instruction: str, task:str, samples: List[List], lang: str, ap
         hyperparams=hyperparams
     )
 
-def execute_code(code: str, inputs: List[str]) -> None:
+@with_api_key
+def execute_code(code: str, task: str, lang: str, instruction: str, inputs: List, samples: List,
+                 router_code: str=None, api_key: str=None, **overrides) -> None:
     """
     Execute the generated code.
     """
-    return None
+    hyperparams = resolve_hyperparams(overrides)
+    return execute_code_logic(
+        code=code,
+        lang=lang,
+        router_code=router_code,
+        instruction=instruction,
+        inputs=inputs,
+        demonstrations=samples,
+        task=task,
+        hyperparams=hyperparams,
+        api_key=api_key)
 
